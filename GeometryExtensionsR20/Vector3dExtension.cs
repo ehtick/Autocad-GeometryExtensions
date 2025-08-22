041ac61a9a451faa0f3e9306c8d9f43e5393260b
Gilles Chanteau <gilecad@orange.fr>
2025-08-22 16:01:11 +0200

    feat: add Vector3d.GetAsPoint mthod

--- GeometryExtensionsR20/Vector3dExtension.cs
+++ GeometryExtensionsR20/Vector3dExtension.cs
@@ -11,7 +11,15 @@ namespace Gile.AutoCAD.R20.Geometry
         /// Projects the point on the WCS XY plane.
         /// </summary>
         /// <param name="vector">The instance to which this method applies.</param>
-        /// <returns>he projected vector.</returns>
+        /// <returns>The projected vector.</returns>
         public static Vector3d Flatten(this Vector3d vector) => new Vector3d(vector.X, vector.Y, 0.0);
+
+        /// <summary>
+        /// Converts 3D vector into 3D point with the same set of coordinates.
+        /// </summary>
+        /// <param name="vector">The instance to which the method applies.</param>
+        /// <returns>An instance of Point3D.</returns>
+        public static Point3d GetAsPoint(this Vector3d vector) =>
+            new Point3d(vector.X, vector.Y, vector.Z);
     }
 }
--- GeometryExtensionsR25/Vector3dExtension.cs
+++ GeometryExtensionsR25/Vector3dExtension.cs
@@ -13,5 +13,12 @@ namespace Gile.AutoCAD.R25.Geometry
         /// <param name="vector">The instance to which this method applies.</param>
         /// <returns>he projected vector.</returns>
         public static Vector3d Flatten(this Vector3d vector) => new(vector.X, vector.Y, 0.0);
+
+        /// <summary>
+        /// Converts 3D vector into 3D point with the same set of coordinates.
+        /// </summary>
+        /// <param name="vector">The instance to which the method applies.</param>
+        /// <returns>An instance of Point3D.</returns>
+        public static Point3d GetAsPoint(this Vector3d vector) => new(vector.X, vector.Y, vector.Z);
     }
 }
